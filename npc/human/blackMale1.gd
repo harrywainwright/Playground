c63eb99228d490fa221dc3f2cd39a6f3f52d4547
harrywainwright <harry.wainwright@live.co.uk>
2023-08-06 15:09:28 +0100

    Lesson 11 - NPC

--- npc/human/blackMale1.gd
+++ npc/human/blackMale1.gd
@@ -7,6 +7,7 @@ extends CharacterBody2D
 
 var startPosition
 var endPosition
+#var rng = RandomNumberGenerator.new()
 
 func _ready():
 	startPosition = position
@@ -17,32 +18,30 @@ func changeDirection():
 	endPosition = startPosition
 	startPosition = temp
 
+# func wait():
+# 	var rng = RandomNumberGenerator.new()
+# 	#randomize end poisition timer
+# 	var rand = rng.randf_range(100, 300)
+# 	#pause npc until changing direction
+# 	await $Timer.rand
+
+	
+
 func updateVelocity():
-	var moveDirection = endPosition - position
+	var moveDirection = (endPosition - position)
 	if moveDirection.length() < limit:
 		changeDirection()
 	velocity = moveDirection.normalized() * speed
 
 func updateAnimation():
+	# if velocity.length() == 0:
+	#  	animations.stop()
+	#  	return
 	var direction = "Down"
-	if velocity.y < 0:
-		direction = "Up"
-
+	if velocity.y < 0: direction = "Up"
 	animations.play("walk" + direction)		
 
-
-
-	# func updateAnimation():
-	# 	if velocity.length() == 0:
-	# 		animations.stop()
-	# 		return
-	# 	var direction = "Down"
-	# 	if velocity.x < 0: direction = "Left"
-	# 	elif velocity.x > 0: direction = "Right"
-	# 	elif velocity.y < 0: direction = "Up"
-	# 	animations.play("walk" + direction)	
-
-func _physics_process(delta):
+func _physics_process(_delta):
 	updateVelocity()
 	move_and_slide()
 	updateAnimation()
